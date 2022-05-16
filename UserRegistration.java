a650971e21430d97641cdfa9b2b822cb0c1a0e45
NiharikaVanapari <niharikavanapari23@gmail.com>
2022-05-16 10:56:12 +0530

    Phone Number

--- UserRegistration.java
+++ UserRegistration.java
@@ -3,35 +3,35 @@ package userregistration;
 import java.util.Scanner;
 import java.util.regex.Pattern;
 
-class UserRegistration 
+public class UserRegistration 
 {
 
-	 Scanner scanner = new Scanner(System.in);
+    Scanner scanner = new Scanner(System.in);
 
-	    public static final String EMAILPATTERN = "([0-9A-Za-z])+([-+._][0-9A-Za-z]+)*" + "@([0-9A-Za-z])+[.]([a-zA-Z])+([.][A-Za-z]+)*";
+    public static final String NUMBERPATTERN = "^[+0-9]{2}{10}$";
 
-	    public static void main(String[] args)
-	    {
-	        UserRegistration userRegistration = new UserRegistration();
-	        userRegistration.readInput();
-	    }
-	    private void readInput() //Method for reading an input.
-	    {
-	        System.out.println("Enter email id ");
-	        String EmailId = scanner.next();
-	        validateInput(EMAILPATTERN,EmailId);
-	    }
+    public static void main(String[] args)
+    {
+        UserRegistration userRegistration = new UserRegistration();
+        userRegistration.readInput();
+    }
+    private void readInput() //Method to read an input.
+    {
+        System.out.println("Enter Phone Number ");
+        String PhoneNumber = scanner.next();
+        validateInput(NUMBERPATTERN,PhoneNumber);
+    }
 
-	    private void validateInput(String pattern, String input)
-	    {
+    private void validateInput(String pattern, String input)
+    {
 
-	        if(Pattern.matches(pattern, input))
-	        {
-	            System.out.println("Valid");
-	        }
-	        else
-	        {
-	            System.out.println("Invalid");
-	        }
-	    }
+        if(Pattern.matches(pattern, input))
+        {
+            System.out.println("Valid");
+        }
+        else
+        {
+            System.out.println("Invalid");
+        }
+    }
 }
