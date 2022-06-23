efc97fd58507b5fd0b63d4a2644a62cc91ea79ab
NiharikaVanapari <niharikavanapari23@gmail.com>
2022-06-23 09:50:19 +0530

    JUnit testing for Sample emails

--- UserRegistration.java
+++ UserRegistration.java
@@ -55,6 +55,42 @@ public class UserRegistration
 	        }
 	    }
 	    
+	    private void SampleemailpatternTest() // Email samples
+		{
+	        String sampleEmail1 = "abc@yahoo.com";
+	        String sampleEmail2 = "abc-100@yahoo.com";
+	        String sampleEmail3 = "abc.100@yahoo.com";
+	        String sampleEmail4 = "abc111@abc.com";
+	        String sampleEmail5 = "abc-100@abc.net";
+	        String sampleEmail6 = "abc.100@abc.com.au";
+	        String sampleEmail7 = "abc@1.com";
+	        String sampleEmail8 = "abc@gmail.com.com";
+	        String sampleEmail9 = "abc+100@gmail.com";
+
+
+	        ArrayList<String> emailIdList = new ArrayList<String>();
+	        emailIdList.add(sampleEmail1);
+	        emailIdList.add(sampleEmail2);
+	        emailIdList.add(sampleEmail3);
+	        emailIdList.add(sampleEmail4);
+	        emailIdList.add(sampleEmail5);
+	        emailIdList.add(sampleEmail6);
+	        emailIdList.add(sampleEmail7);
+	        emailIdList.add(sampleEmail8);
+	        emailIdList.add(sampleEmail9);
+
+	        for (String emailId : emailIdList)
+	        {
+	            if (Pattern.matches(EMAILPATTERN, emailId)) 
+	            {
+	                System.out.println(emailId + " - valid ");
+	            }
+	            else
+	            {
+	                System.out.println(emailId + " - Invalid ");
+	            }
+	        }
+	    }
     
 	    
 	    
@@ -62,40 +98,5 @@ public class UserRegistration
 	    
 	    
 	
-    private void SampleemailpatternTest() // Email samples
-	{
-        String sampleEmail1 = "abc@yahoo.com";
-        String sampleEmail2 = "abc-100@yahoo.com";
-        String sampleEmail3 = "abc.100@yahoo.com";
-        String sampleEmail4 = "abc111@abc.com";
-        String sampleEmail5 = "abc-100@abc.net";
-        String sampleEmail6 = "abc.100@abc.com.au";
-        String sampleEmail7 = "abc@1.com";
-        String sampleEmail8 = "abc@gmail.com.com";
-        String sampleEmail9 = "abc+100@gmail.com";
-
-
-        ArrayList<String> emailIdList = new ArrayList<String>();
-        emailIdList.add(sampleEmail1);
-        emailIdList.add(sampleEmail2);
-        emailIdList.add(sampleEmail3);
-        emailIdList.add(sampleEmail4);
-        emailIdList.add(sampleEmail5);
-        emailIdList.add(sampleEmail6);
-        emailIdList.add(sampleEmail7);
-        emailIdList.add(sampleEmail8);
-        emailIdList.add(sampleEmail9);
-
-        for (String emailId : emailIdList)
-        {
-            if (Pattern.matches(EMAILPATTERN, emailId)) 
-            {
-                System.out.println(emailId + " - valid ");
-            }
-            else
-            {
-                System.out.println(emailId + " - Invalid ");
-            }
-        }
-    }
+  
 }
